1cbdc4c625763bac624291d560eeee2e7bff1901
Karla Abad <kabad1014@gmail.com>
2022-02-14 11:58:55 -0500

    Front end form created

---
+++ client/src/components/ProductForm.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const ProductForm = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <div>
+      <form>
+        <h1>Product Manager</h1>
+        <div>
+          <label>Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Price</label>
+          <input
+            type="number"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Description</label>
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
+};
+
+export default ProductForm;
